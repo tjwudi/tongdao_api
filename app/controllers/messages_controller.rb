c7451e4c5689be12115120ef668f06777a7be4a6
John Wu <webmaster@leapoahead.com>
2013-12-17 12:33:01 +0800

    regular commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,15 @@ class MessagesController < ApplicationController
     @target_user = User.find(params[:id])
     @current_user.send_message(@target_user, params[:content])
   end
+
+  def list_conversations
+    @conversations = current_user.conversations
+    render "messages/conversations"
+  end
+
+  def list_receipts
+    conversation = current_user.conversations.find_by(:user_opp_id => params["id"]) # params["id"] is target user id
+    @receipts = conversation.receipts
+    render "messages/receipts"
+  end
 end
